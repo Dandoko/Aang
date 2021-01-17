b55636bf6adbdf6db93d840dcd2d15b874f4a671
dannywuwu <d246wu@gmail.com>
2021-01-16 22:33:36 -0500

    small depth fixes

--- src/App.js
+++ src/App.js
@@ -260,7 +260,7 @@ function App() {
     } else if ((eyesFurther && useEyes) || (useEars && earsFurther)) {
       setDistance("Far");
     } else {
-      setDistance("Neutral")
+      setDistance("Neutral");
     }
 
     console.log(
@@ -330,11 +330,7 @@ function App() {
           }}
         />
       </header>
-      <Notification
-        bodyPoint={bodyPoint}
-      ></Notification>
-      <DepthNotification distance={distance}
-      ></DepthNotification>
+      <DepthNotification distance={distance}></DepthNotification>
 
       <Flex>
         <Box p="400" bg="red.400">
