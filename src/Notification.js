817ca54d3811920f1c3d89dea478abc4608d7dee
dannywuwu <d246wu@gmail.com>
2021-01-17 12:58:48 -0500

    richard has buggy electron, here you go

--- src/Notification.js
+++ src/Notification.js
@@ -4,11 +4,10 @@ import { Heading } from "@chakra-ui/react";
 
 export default function Notification({ bodyPoint, distance }) {
   //let hasGoodPosture = bodyPoint === "" ? false : <Heading>Please fix your {bodyPoint}</Heading>;
-  let hasGoodDistance = hasGoodPosture === false ? <Heading>Your distance is {distance} </Heading> : false
 
   return (
     <div>
-      {hasGoodDistance}
+      <Heading>Your distance is {distance} </Heading>
       <Model bodyPoint={bodyPoint}></Model>
     </div>
   );
