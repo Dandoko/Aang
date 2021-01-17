9629f5b2d3b7972e872a42fb74aae43b9f97955c
Richard Sun <rsun@tribalscale.com>
2021-01-17 02:57:00 -0500

    removed unnecessary lines  Please enter the commit message for your changes. Lines starting

--- src/Notification.js
+++ src/Notification.js
@@ -3,13 +3,12 @@ import Model from "./Model";
 import { Heading } from "@chakra-ui/react";
 
 export default function Notification({ bodyPoint, distance }) {
-  let hasGoodPosture = bodyPoint === "" ? false : <Heading>Please fix your {bodyPoint}</Heading>;
+  //let hasGoodPosture = bodyPoint === "" ? false : <Heading>Please fix your {bodyPoint}</Heading>;
   let hasGoodDistance = hasGoodPosture === false ? <Heading>Your distance is {distance} </Heading> : false
 
   return (
     <div>
       {hasGoodDistance}
-      {hasGoodPosture}
       <Model bodyPoint={bodyPoint}></Model>
     </div>
   );
